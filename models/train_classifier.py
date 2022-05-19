b8b5030363ead48548cc2e49337c15faee1239e8
jain628 <jainpranjal4444@gmail.com>
2022-05-19 15:47:19 +0530

    final commit

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -50,9 +50,13 @@ def build_model():
     ])
        # parameters set to this due to reduce the size of pkl file, which were too large (600MB) for uploading to github with my previous parameters.
     parameters = {
-    'clf__estimator__n_estimators':[10],
-    'clf__estimator__min_samples_split':[2]
-    }
+                    'clf__estimator__n_estimators': [20],
+                    'clf__estimator__min_samples_split': [5]
+              }
+    #parameters = {
+    #'clf__estimator__n_estimators':[250],
+    #'clf__estimator__min_samples_split':[3]
+    #}
     model = GridSearchCV(pipeline, param_grid=parameters, n_jobs=2, verbose=4)
     return model
     
@@ -95,6 +99,35 @@ def main():
               'save the model to as the second argument. \n\nExample: python '\
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
+
+
+'''def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+'''
 
 if __name__ == '__main__':
     main()
