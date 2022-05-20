625ecfa578b1611038460835d9a556b2c400e1e4
jain628 <jain628>
2022-05-20 15:38:47 +0530

    initial commit for ML model pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,3 +1,4 @@
+#importing libraries
 import sys
 import pandas as pd
 import numpy as np
@@ -19,15 +20,37 @@ nltk.download('punkt')
 nltk.download('wordnet')
 
 def load_data(database_filepath):
+    """
+    Returns two python dataframes and a column after reading DisasterResponse.db from ETL pipeline
+    
+    Parameters:
+        database_filepath (str): a string value for database_filepath
+        
+    Returns:
+        X (python dataframe):a dataframe with messages
+        y (python dataframe): a datframe with all the 36 categories having binary values.
+        category_names(series): a column with category names
+    """
     engine = create_engine('sqlite:///'+database_filepath)
     df = pd.read_sql_table('DisasterResponse',engine)
+    df.drop(df[df['genre'] == 0].index, inplace = True)
     X = df.message
     y = (df[df.columns[4:]]).astype(int)
+    
     category_names = y.columns
     return X, y, category_names 
     
 
 def tokenize(text):
+    """
+    Returns cleaned and lemmatized tokens using regex,WordNetLemmatizer()
+    
+    Parameters:
+        text(str): a string value (from X.message)
+    
+    Returns:
+        clean_tokens (str):a string after tokeninzing,cleaning,lemmatizing
+    """
     url_regex ='http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F]))+'
     detected_urls = re.findall(url_regex,text)
     for url in detected_urls:
@@ -43,6 +66,15 @@ def tokenize(text):
 
 
 def build_model():
+    """
+    Returns a model by implementing CountVectorizer,TfIdfTransformer,MultiOutputClassifier and GridSearchCV .
+    
+    Parameters:
+        None
+    
+    Returns:
+        model : returns GridSearchCV estimator implemented using pipeline.
+    """
     pipeline = Pipeline([
     ('vect',CountVectorizer(tokenizer=tokenize)),
     ('tfidf',TfidfTransformer()),
@@ -50,29 +82,52 @@ def build_model():
     ])
        # parameters set to this due to reduce the size of pkl file, which were too large (600MB) for uploading to github with my previous parameters.
     parameters = {
-                    'clf__estimator__n_estimators': [20],
-                    'clf__estimator__min_samples_split': [5]
+                    'clf__estimator__n_estimators': [10],
+                    'clf__estimator__min_samples_split': [10]
               }
-    #parameters = {
-    #'clf__estimator__n_estimators':[250],
-    #'clf__estimator__min_samples_split':[3]
-    #}
     model = GridSearchCV(pipeline, param_grid=parameters, n_jobs=2, verbose=4)
     return model
     
 
 
 def evaluate_model(model, X_test, y_test, category_names):
-    y_pred = model.predict(X_test)
-    #class_report = classification_report(y_test, y_pred, target_names=category_names)
-    #print(class_report)
+    """
+    Evaluates model by predicting over test data
     
+    Parameters:
+        model(estimator):an estimator returned from build_model() function
+        X_text(python dataframe): a dataframe for train data
+        y_test(python dataframe):a dataframe for test data
+        category_names(python column): column with category names
+    
+    Returns:
+        None
+    """
+    y_pred = model.predict(X_test)
 
 
 def save_model(model, model_filepath):
+    """
+    Saves the model by creating a pickle file named 'classifier.pkl'
+    
+    Parameters:
+        model : model returned by build_model() function
+        model_filepath(str): a string value for the model path
+    Returns:
+        dumps the pickle file.
+       """
     with open(model_filepath, 'wb') as file:
         pickle.dump(model, file)
 def main():
+    """
+    Executes all the functions inside main to generate ML model pipeline
+    
+    Parameters:
+        none
+    
+    Returns:
+        none
+    """
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
@@ -98,36 +153,6 @@ def main():
               'as the first argument and the filepath of the pickle file to '\
               'save the model to as the second argument. \n\nExample: python '\
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
-
-
-
-'''def main():
-    if len(sys.argv) == 3:
-        database_filepath, model_filepath = sys.argv[1:]
-        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
-        print('Building model...')
-        model = build_model()
-        
-        print('Training model...')
-        model.fit(X_train, Y_train)
-        
-        print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
-
-        print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
-
-        print('Trained model saved!')
-
-    else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
-'''
 
 if __name__ == '__main__':
     main()
